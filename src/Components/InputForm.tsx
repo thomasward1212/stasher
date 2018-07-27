859a7ed9580206fccb1f2949bb6b1e44f9b666af
Tom Ward <tom.ward@salecycle.com>
2018-07-27 08:42:20 +0100

    handle enter keypress for search

--- src/Components/InputForm.tsx
+++ src/Components/InputForm.tsx
@@ -5,101 +5,112 @@ import { ListOfStashPoints } from "./ListOfStashPoints";
 import { requestCityStashPoints } from "../utils/requestCityStashPoints";
 
 interface IState {
-	city: string;
-	inputVisible: boolean;
-	data: any;
-	isLoading: boolean;
-	openLate: boolean;
-	showStashPoints: boolean;
+  city: string;
+  inputVisible: boolean;
+  data: any;
+  isLoading: boolean;
+  openLate: boolean;
+  showStashPoints: boolean;
 }
 
 export class InputForm extends React.Component<{}, IState> {
-	public state: IState = {
-		city: "",
-		inputVisible: true,
-		data: [],
-		isLoading: false,
-		openLate: false,
-		showStashPoints: false
-	};
+  public state: IState = {
+    city: "",
+    inputVisible: true,
+    data: [],
+    isLoading: false,
+    openLate: false,
+    showStashPoints: false
+  };
 
-	public render() {
-		return (
-			<div>
-				{this.state.inputVisible ? (
-					<Form>
-						<FormGroup>
-							<h2> Search for five star stashpoints in {this.state.city} </h2>
+  public componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress, false);
+  }
 
-							<Input
-								id="cityInput"
-								placeholder="Enter a city."
-								onChange={this.cityInputUpdated}
-							/>
-							<FormGroup check={true}>
-								<Label check={true}>
-									<Input
-										type="checkbox"
-										onClick={this.onOpenLateCheckboxClick}
-									/>
-									Open late?
-								</Label>
-							</FormGroup>
-						</FormGroup>
+  public render() {
+    return (
+      <div>
+        {this.state.inputVisible ? (
+          <Form>
+            <FormGroup>
+              <h2> Search for five star stashpoints in {this.state.city} </h2>
 
-						<Button
-							color="primary"
-							onClick={this.searchForStashPoints}
-							disabled={this.state.city.length === 0}
-						>
-							Submit
-						</Button>
-					</Form>
-				) : null}
-				{this.state.isLoading ? (
-					<div className="loader">Loading...</div>
-				) : (
-					<div>
-						{this.state.showStashPoints ? (
-							<ListOfStashPoints data={this.state.data} />
-						) : null}
-					</div>
-				)}
-			</div>
-		);
-	}
+              <Input
+                id="cityInput"
+                placeholder="Enter a city."
+                onChange={this.cityInputUpdated}
+                onKeyPress={this.handleKeyPress}
+              />
+              <FormGroup check={true}>
+                <Label check={true}>
+                  <Input
+                    type="checkbox"
+                    onClick={this.onOpenLateCheckboxClick}
+                  />
+                  Open late?
+                </Label>
+              </FormGroup>
+            </FormGroup>
 
-	private cityInputUpdated = (e: any) => {
-		this.setState({
-			city: e.target.value
-		});
-	};
+            <Button
+              color="primary"
+              onClick={this.searchForStashPoints}
+              disabled={this.state.city.length === 0}
+            >
+              Submit
+            </Button>
+          </Form>
+        ) : null}
+        {this.state.isLoading ? (
+          <div className="loader">Loading...</div>
+        ) : (
+          <div>
+            {this.state.showStashPoints ? (
+              <ListOfStashPoints data={this.state.data} />
+            ) : null}
+          </div>
+        )}
+      </div>
+    );
+  }
 
-	private onOpenLateCheckboxClick = (e: any) => {
-		this.setState({
-			openLate: !this.state.openLate
-		});
-	};
+  private cityInputUpdated = (e: any) => {
+    this.setState({
+      city: e.target.value
+    });
+  };
 
-	private searchForStashPoints = (e: any) => {
-		this.setState({
-			city: e.target.value,
-			inputVisible: !this.state.inputVisible
-		}),
-			this.getStashPointsByCity(this.state.city);
-	};
+  private onOpenLateCheckboxClick = () => {
+    this.setState({
+      openLate: !this.state.openLate
+    });
+  };
 
-	private getStashPointsByCity = async (city: string) => {
-		this.setState({
-			isLoading: !this.state.isLoading
-		});
+  private handleKeyPress = (e: any) => {
+    if (e && e.keyCode === 13) {
+      this.searchForStashPoints(e);
+    }
+  };
 
-		const stashpoints = await requestCityStashPoints(city, this.state.openLate);
+  private searchForStashPoints = (e: any) => {
+    this.setState({
+      city: e.target.value,
+      inputVisible: !this.state.inputVisible
+    }),
+      this.getStashPointsByCity(this.state.city);
+  };
 
-		this.setState({
-			data: stashpoints,
-			isLoading: !this.state.isLoading,
-			showStashPoints: !this.state.showStashPoints
-		});
-	};
+  private getStashPointsByCity = async (city: string) => {
+    this.setState({
+      isLoading: !this.state.isLoading
+    });
+
+    const stashpoints = await requestCityStashPoints(city, this.state.openLate);
+
+    this.setState({
+      data: stashpoints,
+      isLoading: !this.state.isLoading,
+      showStashPoints: !this.state.showStashPoints
+    });
+  };
 }
